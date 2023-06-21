8c6d935005dcfb836a67ab9a805d0991e3c889aa
Arslan Majeed <arslanmajeed214@gmail.com>
2023-06-21 22:20:07 +0500

    implmented humberger and desin mobile menu

---
+++ app.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', () => {
+	const menu_btn = document.querySelector('.hamburger');
+	const mobile_menu = document.querySelector('.mobile-nav');
+	const navLinkMenu = document.querySelectorAll('.nav-link');
+
+	menu_btn.addEventListener('click', function () {
+		if(menu_btn.classList.contains('is-active')){
+			menu_btn.classList.remove('is-active');
+			mobile_menu.classList.remove('is-active');
+			document.body.style.overflowY = 'scroll';
+		}else {
+			menu_btn.classList.add('is-active');
+			mobile_menu.classList.add('is-active');
+			document.body.style.overflowY = 'hidden';
+		}
+	});
+
+});
