1c16dcb047518c6f5428c7d116a38ccc44df1722
Arslan Majeed <arslanmajeed214@gmail.com>
2023-06-22 00:42:22 +0500

    Fix some StyleLint and ESLint errors

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
 window.addEventListener('DOMContentLoaded', () => {
-	const menu_btn = document.querySelector('.hamburger');
-	const mobile_menu = document.querySelector('.mobile-nav');
-	const navLinkMenu = document.querySelectorAll('.nav-link');
+  const menuBtn = document.querySelector('.hamburger');
+  const mobileMenu = document.querySelector('.mobile-nav');
+  const navLinkMenu = document.querySelectorAll('.nav-link');
 
-	menu_btn.addEventListener('click', function () {
-		if(menu_btn.classList.contains('is-active')){
-			menu_btn.classList.remove('is-active');
-			mobile_menu.classList.remove('is-active');
-			document.body.style.overflowY = 'scroll';
-		}else {
-			menu_btn.classList.add('is-active');
-			mobile_menu.classList.add('is-active');
-			document.body.style.overflowY = 'hidden';
-		}
-	});
+  menuBtn.addEventListener('click', () => {
+    if (menuBtn.classList.contains('is-active')) {
+      menuBtn.classList.remove('is-active');
+      mobileMenu.classList.remove('is-active');
+      document.body.style.overflowY = 'scroll';
+    } else {
+      menuBtn.classList.add('is-active');
+      mobileMenu.classList.add('is-active');
+      document.body.style.overflowY = 'hidden';
+    }
+  });
 
-	navLinkMenu.forEach((element) => {
-		element.addEventListener('click', () => {
-			menu_btn.classList.remove('is-active');
-			mobile_menu.classList.remove('is-active');
-			document.body.style.overflowY = 'scroll';
-		});
-	});
-
+  navLinkMenu.forEach((element) => {
+    element.addEventListener('click', () => {
+      menuBtn.classList.remove('is-active');
+      mobileMenu.classList.remove('is-active');
+      document.body.style.overflowY = 'scroll';
+    });
+  });
 });
