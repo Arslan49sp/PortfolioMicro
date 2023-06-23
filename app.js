76728500e3ac905a6a2056805a752a1fe45dd87a
Arslan Majeed <arslanmajeed214@gmail.com>
2023-06-24 00:37:29 +0500

    Fixed some styling issues of tech stack

--- app.js
+++ app.js
@@ -87,8 +87,8 @@ const portfolioList = [
   },
 ];
 
-function cardAndModal(portfolio) {
-  const techStack = portfolio.techStack
+function cardAndModal(project) {
+  const techStack = project.techStack
     .map(
       (element) =>
         `<li class="tool_item"><button class="tools_design">${element}</button></li>`
@@ -96,28 +96,28 @@ function cardAndModal(portfolio) {
     .join("");
 
   return `<div class="project_card">
-  <div class="img_holder" style="--image:url('${portfolio.screenshotMobile}')"></div>
-  <h2 class="large_title">${portfolio.title}</h2>
+  <div class="img_holder" style="--image:url('${project.screenshotMobile}')"></div>
+  <h2 class="large_title">${project.title}</h2>
   <ul class="tools_flex flex_center">
    ${techStack}
   </ul>
-  <button class="btngreen" data-modal="modal${portfolio.id}">See project</button>
+  <button class="btngreen" data-modal="modal${project.id}">See project</button>
 </div>
 </div>
-<dialog id="modal${portfolio.id}">
+<dialog id="modal${project.id}">
 <div class="modal">
-<button class="close_modal_btn" data-modal="modal${portfolio.id}"><i class="fas fa-xmark fa-lg"></i></button>
-<div class="modal_image" style="--imageMobile:url('${portfolio.screenshotMobile}')"></div>
-<h3 class="modal-title">${portfolio.title}</h3>
+<button class="close_modal_btn" data-modal="modal${project.id}"><i class="fas fa-xmark fa-lg"></i></button>
+<div class="modal_image" style="--imageMobile:url('${project.screenshotMobile}')"></div>
+<h3 class="modal_title">${project.title}</h3>
 <div><ul class="flex_left tools_flex">
   ${techStack}
 </ul></div>
-<p class="text_left">${portfolio.details}</p>
-<div class="modal-button-container">
-<a class="btngreen no_decoration" href=${portfolio.live} target="_blank">
+<p class="text_left">${project.details}</p>
+<div class="modal_btn_container">
+<a class="btngreen no_decoration" href=${project.live} target="_blank">
 See live <span><i class="fa-duotone fa-arrow-up-left-from-circle fa-rotate-90 fa-xl fa-fw"></i></span>
 </a>
-<a class="btngreen no_decoration" href=${portfolio.source} target="_blank">
+<a class="btngreen no_decoration" href=${project.source} target="_blank">
 See source <span><i class="fa-brands fa-github fa-xl fa-fw"></i></span>
 </a>
 </div>
