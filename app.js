595ef878d5b4596aab1bba4a16bdc86fda32cb0e
Arslan Majeed <arslanmajeed214@gmail.com>
2023-06-24 00:43:14 +0500

    Fix some StyleLint and ESlint related errors|

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const menuBtn = document.querySelector(".hamburger");
-  const mobileMenu = document.querySelector(".mobile-nav");
-  const navLinkMenu = document.querySelectorAll(".nav-link");
+window.addEventListener('DOMContentLoaded', () => {
+  const menuBtn = document.querySelector('.hamburger');
+  const mobileMenu = document.querySelector('.mobile-nav');
+  const navLinkMenu = document.querySelectorAll('.nav-link');
 
-  menuBtn.addEventListener("click", () => {
-    if (menuBtn.classList.contains("is-active")) {
-      menuBtn.classList.remove("is-active");
-      mobileMenu.classList.remove("is-active");
-      document.body.style.overflowY = "scroll";
+  menuBtn.addEventListener('click', () => {
+    if (menuBtn.classList.contains('is-active')) {
+      menuBtn.classList.remove('is-active');
+      mobileMenu.classList.remove('is-active');
+      document.body.style.overflowY = 'scroll';
     } else {
-      menuBtn.classList.add("is-active");
-      mobileMenu.classList.add("is-active");
-      document.body.style.overflowY = "hidden";
+      menuBtn.classList.add('is-active');
+      mobileMenu.classList.add('is-active');
+      document.body.style.overflowY = 'hidden';
     }
   });
 
   navLinkMenu.forEach((element) => {
-    element.addEventListener("click", () => {
-      menuBtn.classList.remove("is-active");
-      mobileMenu.classList.remove("is-active");
-      document.body.style.overflowY = "scroll";
+    element.addEventListener('click', () => {
+      menuBtn.classList.remove('is-active');
+      mobileMenu.classList.remove('is-active');
+      document.body.style.overflowY = 'scroll';
     });
   });
 });
@@ -27,73 +27,72 @@ window.addEventListener("DOMContentLoaded", () => {
 const portfolioList = [
   {
     id: 1,
-    title: "Online graphics tool for designers",
-    techStack: ["HTML", "JavaScript", "Ruby on Rails"],
-    screenshotMobile: "assets/model.png",
+    title: 'Online graphics tool for designers',
+    techStack: ['HTML', 'JavaScript', 'Ruby on Rails'],
+    screenshotMobile: 'assets/model.png',
     details:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s",
-    source: "#",
-    live: "#",
+    source: '#',
+    live: '#',
   },
   {
     id: 2,
-    title: "Multi-Post Stories Gain+Glory One",
-    techStack: ["HTML", "JavaScript", "Ruby on Rails"],
-    screenshotMobile: "#",
+    title: 'Multi-Post Stories Gain+Glory One',
+    techStack: ['HTML', 'JavaScript', 'Ruby on Rails'],
+    screenshotMobile: '#',
     details:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s",
-    source: "#",
-    live: "#",
+    source: '#',
+    live: '#',
   },
   {
     id: 3,
-    title: "Multi-Post Stories Gain+Glory Two",
-    techStack: ["HTML", "JavaScript", "Ruby on Rails"],
-    screenshotMobile: "#",
+    title: 'Multi-Post Stories Gain+Glory Two',
+    techStack: ['HTML', 'JavaScript', 'Ruby on Rails'],
+    screenshotMobile: '#',
     details:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s",
-    source: "#",
-    live: "#",
+    source: '#',
+    live: '#',
   },
   {
     id: 4,
-    title: "Multi-Post Stories Gain+Glory Three",
-    techStack: ["HTML", "JavaScript", "Ruby on Rails"],
-    screenshotMobile: "#",
+    title: 'Multi-Post Stories Gain+Glory Three',
+    techStack: ['HTML', 'JavaScript', 'Ruby on Rails'],
+    screenshotMobile: '#',
     details:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s",
-    source: "#",
-    live: "#",
+    source: '#',
+    live: '#',
   },
   {
     id: 5,
-    title: "Multi-Post Stories Gain+Glory Four",
-    techStack: ["HTML", "JavaScript", "Ruby on Rails"],
-    screenshotMobile: "#",
+    title: 'Multi-Post Stories Gain+Glory Four',
+    techStack: ['HTML', 'JavaScript', 'Ruby on Rails'],
+    screenshotMobile: '#',
     details:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s",
-    source: "#",
-    live: "#",
+    source: '#',
+    live: '#',
   },
   {
     id: 6,
-    title: "Multi-Post Stories Gain+Glory Five",
-    techStack: ["HTML", "JavaScript", "Ruby on Rails"],
-    screenshotMobile: "#",
+    title: 'Multi-Post Stories Gain+Glory Five',
+    techStack: ['HTML', 'JavaScript', 'Ruby on Rails'],
+    screenshotMobile: '#',
     details:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s",
-    source: "#",
-    live: "#",
+    source: '#',
+    live: '#',
   },
 ];
 
 function cardAndModal(project) {
   const techStack = project.techStack
     .map(
-      (element) =>
-        `<li class="tool_item"><button class="tools_design">${element}</button></li>`
+      (element) => `<li class="tool_item"><button class="tools_design">${element}</button></li>`,
     )
-    .join("");
+    .join('');
 
   return `<div class="project_card">
   <div class="img_holder" style="--image:url('${project.screenshotMobile}')"></div>
@@ -131,22 +130,22 @@ const cards = () => {
   return card;
 };
 
-document.querySelector(".work_card_container").innerHTML = cards().join("");
+document.querySelector('.work_card_container').innerHTML = cards().join('');
 
-const modalButtons = document.querySelectorAll(".btngreen");
-const closeButtons = document.querySelectorAll(".close_modal_btn");
+const modalButtons = document.querySelectorAll('.btngreen');
+const closeButtons = document.querySelectorAll('.close_modal_btn');
 
 if (closeButtons) {
   closeButtons.forEach((element) => {
-    const modalId = element.getAttribute("data-modal");
+    const modalId = element.getAttribute('data-modal');
     const modal = document.getElementById(`${modalId}`);
 
-    element.addEventListener("click", () => {
+    element.addEventListener('click', () => {
       modal.close();
     });
 
-    modal.addEventListener("close", () => {
-      document.body.style.overflowY = "scroll";
+    modal.addEventListener('close', () => {
+      document.body.style.overflowY = 'scroll';
     });
   });
 }
@@ -154,12 +153,12 @@ if (closeButtons) {
 if (modalButtons) {
   for (let index = 0; index < modalButtons.length; index += 1) {
     const element = modalButtons[index];
-    const modalId = element.getAttribute("data-modal");
+    const modalId = element.getAttribute('data-modal');
     const modal = document.getElementById(`${modalId}`);
 
-    element.addEventListener("click", () => {
+    element.addEventListener('click', () => {
       modal.showModal();
-      document.body.style.overflowY = "hidden";
+      document.body.style.overflowY = 'hidden';
     });
   }
 }
